9b6a1bbe90d5139e514b72057645f1fc81b70626
cmcmurrough <christopher.mcmurrough@gmail.com>
2019-12-01 22:49:51 -0600

    update

--- esp8266/access_point_http_server/access_point_http_server.ino
+++ esp8266/access_point_http_server/access_point_http_server.ino
@@ -7,12 +7,16 @@
 // create a webserver running on port 80
 ESP8266WebServer server(80);
 
+// define pin settings
+const int LED_PIN = 16;
+
 // define access point settings
 const char * ssid = "ESP8266 Access Point";
 const char * password = "esp8266arduino";
 
 // define function prototypes for HTTP handlers
 void handleRoot();
+void handleLED();
 void handleNotFound();
 
 void setup() 
@@ -21,6 +25,9 @@ void setup()
     Serial.begin(115200);
     delay(10);
     Serial.println('\n');
+
+    // setup LED pin
+    pinMode(LED_PIN, OUTPUT);
 
     // start the access point
     WiFi.softAP(ssid, password);
@@ -34,6 +41,7 @@ void setup()
 
     // start handlers for HTTP requests
     server.on("/", handleRoot);
+    server.on("/LED", HTTP_POST, handleLED);
     server.onNotFound(handleNotFound);
 
     // start the server
@@ -53,6 +61,16 @@ void handleRoot()
 {
     // send response message with 200 HTTP status code
     server.send(200, "text/plain", "Hello world!");
+}
+
+// handler for requests at LED path
+void handleLED() 
+{                          
+    // change LED state
+  digitalWrite(LED_PIN,!digitalRead(LED_PIN));
+
+  // send the response
+  server.send(200, "text/plain", "LED state changed!");
 }
 
 // handler for undefined paths
